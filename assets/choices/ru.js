f99003e1d28640ce7a876a4a4c5f5797548e8391
TheRealGranchio <niccologranchi@gmail.com>
2026-01-07 17:31:23 +0100

    Add files via upload

---
+++ assets/choices/ru.js
@@ -0,0 +1,673 @@
+ru={
+    "2G": {
+        "1R": "PHAEDRA",
+        "1S": "THE BERMUDA TRIANGLE"
+    },
+    "3AF2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3AF1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "SS20": {
+        "R5": "VISIT DR HAYNES",
+        "3Nx": "GO BACK"
+    },
+    "SS21": {
+        "R6": "PAC",
+        "5H": "KILL DAD"
+    },
+    "SS22": {
+        "nsg-WhoThereChoice": "WHO'S THERE?",
+        "5H": "KILL DAD"
+    },
+    "2Bp2": {
+        "No": "NO"
+    },
+    "3Xcd": {
+        "nsg-ThrowThemChoice": "BUTTALE VIA",
+        "nsg-FlushThemChoice3X": "BUTTALE NEL WATER"
+    },
+    "2Bp1": {
+        "No": "NO",
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "Z62f": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3Vx": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "S44A": {
+        "R3": "PARLA DELLA MADRE",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "Z62e": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "S44B": {
+        "R3": "PARLA DELLA MADRE",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "Z62d": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "7D": {
+        "Yeah": "SÌ",
+        "FYeah": "FUCK YEAH",
+        "7L": "LEAP THROUGH THE WINDOW",
+        "7H": "FIGHT HER"
+    },
+    "Z62c": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3Vff": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "1H": {
+        "nsg-8AChoice": "ACCETTA",
+        "nsg-1Qnw-1Qtt": "RIFIUTA"
+    },
+    "ZQ": {
+        "nsg-FamilyPhotoChoice": "PRENDI LA FOTO DI FAMIGLIA",
+        "nsg-BookChoice": "PRENDI IL LIBRO"
+    },
+    "1G": {
+        "nsg-8AChoice": "ACCETTA",
+        "nsg-1Qnw-1Qtt": "RIFIUTA"
+    },
+    "ZP": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "5AG2": {
+        "5AH": "LET HIM GO",
+        "5AJ": "KILL HIM"
+    },
+    "SS16": {
+        "R3": "PARLA DELLA MADRE",
+        "3Vx": "GO BACK"
+    },
+    "SS17": {
+        "R4": "SEGUI COLIN",
+        "3Vx": "GO BACK"
+    },
+    "1S": {
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "SS18": {
+        "3Vx": "GO BACK"
+    },
+    "1R": {
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "SS19": {
+        "R3": "PARLA DELLA MADRE",
+        "3Nx": "GO BACK"
+    },
+    "Z62b": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "Z62a": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "2Bt1": {
+        "No": "NO",
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "3Vfs": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "SS10": {
+        "1PA": "GO BACK"
+    },
+    "SS11": {
+        "nsg-SS11-0": "WORK AT TUCKERSOFT?",
+        "3Ax": "GO BACK"
+    },
+    "SS12": {
+        "R3": "PARLA DELLA MADRE",
+        "3Ax": "GO BACK"
+    },
+    "SS13": {
+        "3Ax": "GO BACK"
+    },
+    "SS14": {
+        "R3": "PARLA DELLA MADRE",
+        "nsg-SS14-1": "GO BACK"
+    },
+    "SS15": {
+        "R4": "SEGUI COLIN",
+        "nsg-SS15-1": "GO BACK"
+    },
+    "3Nx": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "2B": {
+        "No": "NO",
+        "1R": "PHAEDRA",
+        "1S": "THE BERMUDA TRIANGLE"
+    },
+    "S35B": {
+        "nsg-WhoThereChoice": "WHO'S THERE?",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "2Gp1": {
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "3AH2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3Wbs": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "1HB": {
+        "nsg-8AChoice": "ACCETTA",
+        "nsg-1Q_rewatch": "RIFIUTA"
+    },
+    "3AH1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "S59A": {
+        "nsg-S59A-0": "COSA  DEVO FARE?",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "S59B": {
+        "nsg-S59B-0": "COSA  DEVO FARE?",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "S35A": {
+        "nsg-WhoThereChoice": "WHO'S THERE?",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "5A": {
+        "5H": "KILL DAD",
+        "5G": "BACK OFF"
+    },
+    "5QA": {
+        "5UA": "SÌ",
+        "5TA": "NO"
+    },
+    "1QA": {
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "8JB1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "S34A": {
+        "R4": "SEGUI COLIN",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "3N": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "8JB2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "S34B": {
+        "R4": "SEGUI COLIN",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "1Qtt": {
+        "nsg-2BChoice": "SÌ",
+        "2GA": "NO"
+    },
+    "3M": {
+        "3N": "SÌ",
+        "3P": "NO"
+    },
+    "3L": {
+        "nsg-VisitHaynesChoice": "VISIT DR. HAYNES",
+        "nsg-FollowColinChoice": "SEGUI COLIN"
+    },
+    "3J": {
+        "nsg-VisitHaynesChoice": "VISIT DR. HAYNES",
+        "nsg-FollowColinChoice": "SEGUI COLIN"
+    },
+    "5AD": {
+        "5AF": "NO IDEA",
+        "5AE": "HE JUMPED"
+    },
+    "3R": {
+        "3U": "BITE NAILS",
+        "3T": "PULL EARLOBE"
+    },
+    "3P": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "3Z": {
+        "nsg-FamilyPhotoChoice": "PRENDI LA FOTO DI FAMIGLIA",
+        "nsg-BookChoice": "PRENDI IL LIBRO"
+    },
+    "3X": {
+        "8B_Variant2": "PRENDILE",
+        "nsg-FlushThemChoice3X": "BUTTALE NEL WATER"
+    },
+    "3VA": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "3Wbf": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "ZK1": {
+        "8J": "TOY",
+        "nsg-PAXChoice": "PAX"
+    },
+    "ZK2": {
+        "nsg-PAXChoice": "PAX",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "5AG": {
+        "5AH": "LET HIM GO",
+        "5AJ": "KILL HIM"
+    },
+    "ZK3": {
+        "8J": "TOY",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "3VB": {
+        "nsg-FamilyPhotoChoice": "PRENDI LA FOTO DI FAMIGLIA",
+        "nsg-BookChoice": "PRENDI IL LIBRO"
+    },
+    "3AB2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3AB1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "2Bt2": {
+        "No": "NO"
+    },
+    "3AJ2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3AJ1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "S37A": {
+        "R2": "GET RABBIT FROM DAD",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "S37B": {
+        "R2": "GET RABBIT FROM DAD",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "5AD2": {
+        "5AF": "NO IDEA",
+        "5AE": "HE JUMPED"
+    },
+    "3Xxc": {
+        "8B": "BUTTALE VIA",
+        "nsg-FlushThemChoice3X": "BUTTALE NEL WATER"
+    },
+    "7B": {
+        "7C": "TELL HIM MORE",
+        "7D": "STOP THE CONVERSATION"
+    },
+    "7A": {
+        "7B": "TELL HIM MORE",
+        "7B2": "TRY TO EXPLAIN"
+    },
+    "3Xxa": {
+        "8B_Variant2": "PRENDILE",
+        "nsg-FlushThemChoice3X": "BUTTALE NEL WATER"
+    },
+    "3Ax": {
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "3AK1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "7H": {
+        "7K": "KARATE CHOP DAD",
+        "7J": "KICK HIM IN THE BALLS"
+    },
+    "5Q": {
+        "5U": "SÌ",
+        "5T": "NO"
+    },
+    "SS7": {
+        "R1": "PICK UP PHOTO",
+        "5H": "KILL DAD"
+    },
+    "SS8": {
+        "R2": "GET RABBIT FROM DAD",
+        "5H": "KILL DAD"
+    },
+    "SS9": {
+        "R4": "SEGUI COLIN",
+        "5H": "KILL DAD"
+    },
+    "SS3": {
+        "R2": "GET RABBIT FROM DAD",
+        "nsg-WhoThereChoice": "WHO'S THERE?"
+    },
+    "SS4": {
+        "R4": "SEGUI COLIN",
+        "nsg-WhoThereChoice": "WHO'S THERE?"
+    },
+    "SS5": {
+        "nsg-WhoThereChoice": "WHO'S THERE?"
+    },
+    "SS6": {
+        "R3": "PARLA DELLA MADRE",
+        "5H": "KILL DAD"
+    },
+    "1GB": {
+        "nsg-8AChoice": "ACCETTA",
+        "nsg-1Q_rewatch": "RIFIUTA"
+    },
+    "SS1": {
+        "R3": "PARLA DELLA MADRE",
+        "R4": "SEGUI COLIN"
+    },
+    "1GA": {
+        "nsg-8AChoice": "ACCETTA",
+        "nsg-1Q_rewatch": "RIFIUTA"
+    },
+    "5V": {
+        "nsg-5V-0": "SEPPELLISCILO",
+        "nsg-5V-1": "FAI A PEZZI IL CORPO"
+    },
+    "SS2": {
+        "R1": "PICK UP PHOTO",
+        "nsg-WhoThereChoice": "WHO'S THERE?"
+    },
+    "S36A": {
+        "R1": "PICK UP PHOTO",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "S36B": {
+        "R1": "PICK UP PHOTO",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "SS52": {
+        "nsg-SS52-0": "COSA  DEVO FARE?",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "SS53": {
+        "nsg-SS53-0": "COSA  DEVO FARE?",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "SS54": {
+        "nsg-WhoThereChoice-SS54": "WHO'S THERE?",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "SS55": {
+        "nsg-SS55-0": "PICK UP PHOTO",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "1HA": {
+        "nsg-8AChoice": "ACCETTA",
+        "nsg-1Q_rewatch": "RIFIUTA"
+    },
+    "SS56": {
+        "nsg-SS56-0": "GET RABBIT FROM DAD",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "3Vbs": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "SS57": {
+        "R6": "PAC",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "3Vbf": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "1PA": {
+        "nsg-8AChoice": "ACCETTA",
+        "nsg-1Q_rewatch": "RIFIUTA"
+    },
+    "1Qnw": {
+        "nsg-2BChoice": "SÌ",
+        "2GA": "NO"
+    },
+    "Z4cd": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "SS50": {
+        "nsg-SS50-0": "PARLA DELLA MADRE",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "SS51": {
+        "nsg-SS51-0": "SEGUI COLIN",
+        "0cr2": "GUARDA I TITOLI DI CODA"
+    },
+    "Z61e": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3AD3": {
+        "8J": "TOY",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "Z61d": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "Z4": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "Z61c": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "Z61b": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "Z1": {
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "Z61f": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "Z61a": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3AD2": {
+        "nsg-PAXChoice": "PAX",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "3AD1": {
+        "8J": "TOY",
+        "nsg-PAXChoice": "PAX"
+    },
+    "ZE": {
+        "nsg-FamilyPhotoChoice": "PRENDI LA FOTO DI FAMIGLIA",
+        "nsg-BookChoice": "PRENDI IL LIBRO"
+    },
+    "ZD": {
+        "nsg-FamilyPhotoChoice": "PRENDI LA FOTO DI FAMIGLIA",
+        "nsg-BookChoice": "PRENDI IL LIBRO"
+    },
+    "4C2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "ZB": {
+        "nsg-2BChoice": "SÌ",
+        "2GA": "NO"
+    },
+    "1E": {
+        "1H": "THOMPSON TWINS",
+        "1G": "NOW 2"
+    },
+    "3Xac": {
+        "nsg-ThrowThemChoice": "BUTTALE VIA",
+        "nsg-FlushThemChoice3X": "BUTTALE NEL WATER"
+    },
+    "1D": {
+        "1H": "THOMPSON TWINS",
+        "1G": "NOW 2"
+    },
+    "3Xa": {
+        "8B_Variant2": "PRENDILE",
+        "nsg-FlushThemChoice3X": "BUTTALE NEL WATER"
+    },
+    "ZK": {
+        "nsg-JFDChoice": "JFD",
+        "nsg-PAXChoice": "PAX"
+    },
+    "1A": {
+        "1E": "SUGAR PUFFS",
+        "1D": "FROSTIES"
+    },
+    "4C1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "ZJ": {
+        "nsg-FamilyPhotoChoice": "PRENDI LA FOTO DI FAMIGLIA",
+        "nsg-BookChoice": "PRENDI IL LIBRO"
+    },
+    "ZH": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "ZG": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "3AD": {
+        "nsg-JFDChoice": "JFD",
+        "nsg-PAXChoice": "PAX"
+    },
+    "3AC2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3AC": {
+        "nsg-ThrowTeaChoice": "ROVESCIA IL THE SUL PC",
+        "nsg-DestroyComputerChoice": "DISTRUGGI IL PC"
+    },
+    "3AB": {
+        "nsg-ThrowTeaChoice": "ROVESCIA IL THE SUL PC",
+        "nsg-DestroyComputerChoice": "DISTRUGGI IL PC"
+    },
+    "ZHw6": {
+        "nsg-FamilyPhotoChoice": "PRENDI LA FOTO DI FAMIGLIA",
+        "nsg-BookChoice": "PRENDI IL LIBRO"
+    },
+    "3AH": {
+        "nsg-ThrowTeaChoice": "ROVESCIA IL THE SUL PC",
+        "nsg-DestroyComputerChoice": "DISTRUGGI IL PC"
+    },
+    "3Wff": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "3AF": {
+        "nsg-ThrowTeaChoice": "ROVESCIA IL THE SUL PC",
+        "nsg-DestroyComputerChoice": "DISTRUGGI IL PC"
+    },
+    "3AK2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "2GA": {
+        "nsg-2BChoice": "SÌ",
+        "nsg-2GChoice": "NO"
+    },
+    "S38A": {
+        "R6": "PAC",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "S38B": {
+        "R6": "PAC",
+        "0cr1": "GUARDA I TITOLI DI CODA"
+    },
+    "3AC1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "2Gt1": {
+        "3B": "ROVESCIA IL THE SUL PC",
+        "3C": "GRIDA CONTRO PAPÀ"
+    },
+    "3AL2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3Wfs": {
+        "3Y": "DISTRUGGI IL PC",
+        "3Z": "DAI UN COLPO ALLA SCRIVANIA"
+    },
+    "3AL1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "3AL": {
+        "nsg-ThrowTeaChoice": "ROVESCIA IL THE SUL PC",
+        "nsg-DestroyComputerChoice": "DISTRUGGI IL PC"
+    },
+    "3AK": {
+        "nsg-ThrowTeaChoice": "ROVESCIA IL THE SUL PC",
+        "nsg-DestroyComputerChoice": "DISTRUGGI IL PC"
+    },
+    "8K": {
+        "nsg-8K-0": "ROVESCIA IL THE SUL PC",
+        "nsg-8K-1": "DISTRUGGI IL PC"
+    },
+    "3AJ": {
+        "nsg-ThrowTeaChoice": "ROVESCIA IL THE SUL PC",
+        "nsg-DestroyComputerChoice": "DISTRUGGI IL PC"
+    },
+    "8J": {
+        "8JA": "SÌ",
+        "nsg-8J-1": "NO"
+    },
+    "4D2": {
+        "7A": "NETFLIX",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "4D1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "'WHITE BEAR"
+    },
+    "5VA": {
+        "nsg-5V-0": "SEPPELLISCILO",
+        "nsg-5V-1": "FAI A PEZZI IL CORPO"
+    }
+}
